202a45d6223317bfd2ac0ec65e08a044e085dabc
Christoph Budziszewski <christoph@budziszewski.de>
2024-12-07 17:42:41 +0100

    Day 7 Part 2

---
+++ Day07/part2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+calibrations = list()
+targets = list()
+with open('input.txt') as file:
+    for line in file:
+        k, v = line.rstrip().split(":")
+        targets.append(k)
+        calibrations.append(list(map(int,v.split())))
+        print(targets)
+        print(calibrations)
+
+operators = list()
+operators.append(lambda a, b: a*b)
+operators.append(lambda a, b: a+b)
+operators.append(lambda a, b: int(''.join([str(a), str(b)])))
+
+print('########')
+
+def is_calibration_correct(target, running, remaining):
+    if len(remaining) == 1:
+        result = [op(running, remaining[0]) for op in operators]
+        print(f"result: {[int(r) == int(target) for r in result]}")
+        return any([r == target for r in result])
+    else:
+        return any([ is_calibration_correct(target, op(running, remaining[0]), remaining[1:]) for op in operators ])
+
+## liste umkehren und rekursiv bearbeiten
+## das liest sich aber wie ein endrekursives problem
+
+# Zero can be returned by any wrong calc
+total_calibration_result = 0
+for i, v in enumerate(calibrations):
+    print(f"cal {i}: {v} tgt {targets[i]}")
+    correct = is_calibration_correct(int(targets[i]), v[0], v[1:])
+    total_calibration_result = total_calibration_result + int(targets[i]) if correct else total_calibration_result
+    print(f"total: {total_calibration_result}")
+
+print(f"total: {total_calibration_result}")
+
+
+
+
